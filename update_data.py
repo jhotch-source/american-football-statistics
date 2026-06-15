a660ec4565d4ff0335f341a6dd8959c518fc492b
jhotch-source <goaliestrike120@gmail.com>
2026-06-14 23:39:13 -0400

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -5,8 +5,8 @@ print("Downloading player stats...")
 player_url = "https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats.csv"
 player_df = pd.read_csv(player_url)
 
-latest_season = df["season"].max()
-df = df[df["season"] >= latest_season - 2]
+latest_season = player_df["season"].max()
+player_df = player_df[player_df["season"] >= latest_season - 2]
 player_df.to_csv("nfl_data.csv", index=False)
 
 print(f"Saved {len(player_df)} player stat records.")
